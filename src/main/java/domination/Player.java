0cad62e20ef769b74e15b0f5460643402f6a0c98
marc <marc.partensky@gmail.com>
2021-05-25 19:09:14 +0200

    linking objects together

--- src/main/java/domination/Player.java
+++ src/main/java/domination/Player.java
@@ -1,12 +1,21 @@
 package domination;
 
+/*
+ * Main player class.
+ */
 public class Player {
 
+	/*
+	 * Build a player with default arguments.
+	 */
 	static public Player build() {
 		return new Player();
 	}
 
+	/*
+	 * Initialize a player.
+	 */
 	public Player() {
-
+		// put something in here
 	}
 }
